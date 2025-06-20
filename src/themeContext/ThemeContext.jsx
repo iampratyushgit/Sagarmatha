694b41b8f8016a5f65b644362f752894be93f9a6
iampratyushgit <pokharelpratyush620@gmail.com>
2025-06-20 12:42:06 +0545

    refactor: Remove unnecessary blank line in ThemeContext component

--- src/themeContext/ThemeContext.jsx
+++ src/themeContext/ThemeContext.jsx
@@ -1,6 +1,5 @@
 import React, { createContext, useState } from "react";
 
-
 export const ThemeContext = createContext();
 export function ThemeProvider({ children }) {
     const [theme, setTheme] = useState("Light");
